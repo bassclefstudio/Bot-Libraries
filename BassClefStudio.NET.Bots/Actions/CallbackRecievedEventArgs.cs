66ea53f57c2a78f91b7a4027c76762d1012f50e7
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-11-03 00:17:00 -0500

    Added Helpers and Actions documentation.

--- BassClefStudio.NET.Bots/Actions/CallbackRecievedEventArgs.cs
+++ BassClefStudio.NET.Bots/Actions/CallbackRecievedEventArgs.cs
@@ -5,11 +5,26 @@ using System.Text;
 
 namespace BassClefStudio.NET.Bots.Actions
 {
+    /// <summary>
+    /// An event called when a <see cref="CallbackBotAction"/> action is activated by a user.
+    /// </summary>
     public class CallbackReceivedEventArgs : EventArgs
     {
+        /// <summary>
+        /// The related <see cref="CallbackBotAction"/>.
+        /// </summary>
         public CallbackBotAction CallbackAction { get; }
+
+        /// <summary>
+        /// The <see cref="BotChat"/> that sent the callback.
+        /// </summary>
         public BotChat FromChat { get; }
 
+        /// <summary>
+        /// Creates a new <see cref="CallbackReceivedEventArgs"/>.
+        /// </summary>
+        /// <param name="action">The related <see cref="CallbackBotAction"/>.</param>
+        /// <param name="fromChat">The <see cref="BotChat"/> that sent the callback.</param>
         public CallbackReceivedEventArgs(CallbackBotAction action, BotChat fromChat)
         {
             CallbackAction = action;
--- BassClefStudio.NET.Bots/Actions/IBotAction.cs
+++ BassClefStudio.NET.Bots/Actions/IBotAction.cs
@@ -1,4 +1,5 @@
 ﻿using BassClefStudio.NET.Bots.Content;
+using BassClefStudio.NET.Bots.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,10 +7,24 @@ using System.Threading.Tasks;
 
 namespace BassClefStudio.NET.Bots.Actions
 {
+    /// <summary>
+    /// Represents an action presented to the user by the <see cref="Bot"/> that can prompt a response at a later time.
+    /// </summary>
     public interface IBotAction
     {
+        /// <summary>
+        /// A unique ID for the <see cref="IBotAction"/>.
+        /// </summary>
         string Id { get; }
+
+        /// <summary>
+        /// The name of the action button, given to the user.
+        /// </summary>
         string DisplayName { get; }
+
+        /// <summary>
+        /// A description of the <see cref="IBotAction"/>, sometimes shown to the user.
+        /// </summary>
         string Description { get; }
     }
 }
